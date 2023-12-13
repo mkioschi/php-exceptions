3e3b8b4427d85c5f7070b8c54380b902754d0f57
Mayko Kioschi <mkioschi@gmail.com>
2023-12-13 20:44:26 -0300

    Initial commit

---
+++ src/Exceptions/Http/InvalidTypeHttpException.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Mkioschi\Exceptions\Http;
+
+use Mkioschi\Enums\HttpStatus;
+use Throwable;
+
+class InvalidTypeHttpException extends HttpException
+{
+    /** @var ?string[] */
+    private ?array $errors;
+
+    /**
+     * @param string $message
+     * @param ?int $code
+     * @param ?array $errors
+     * @param ?Throwable $previous
+     */
+    public function __construct(
+        string $message = 'Invalid value.',
+        ?int $code = null,
+        ?array $errors = null,
+        ?Throwable $previous = null,
+    )
+    {
+        $this->errors = $errors;
+        parent::__construct(
+            message: $message,
+            statusCode: HttpStatus::UNPROCESSABLE_ENTITY,
+            code: $code,
+            previous: $previous
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getErrors(): array
+    {
+        if (is_null($this->errors)) return [];
+        return $this->errors;
+    }
+}
